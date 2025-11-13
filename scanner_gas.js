5449f9dacdc7053bac6da6ea9770475f152f1f69
drekkg <rizzagum@gmail.com>
2025-11-13 22:23:54 +0100

    Update HTML structure and add technician input functionality in the scanner app

--- scanner_gas.js
+++ scanner_gas.js
@@ -1,12 +1,27 @@
+document.getElementById("technician").focus();
+const currentDate = new Date().toISOString().slice(0, 10);
 
 
-document.getElementById("barcode").addEventListener("keydown", (event) => {
-  let barcode_value = document.getElementById("barcode").value;
+
+
+document.getElementById("technician").addEventListener("keydown", (event) => {
+  let technician_value = document.getElementById("technician").value;
+  if (event.key === "Enter") {
+    const technician_html = document.createElement("h4");
+    technician_html.textContent = `Techniker: ${technician_value}  Datum: ${currentDate}`;
+    document.getElementById("barcode-container").appendChild(technician_html);
+    document.getElementById("technician").value = "";
+    document.getElementById("technician_input").remove();
+    document.getElementById("barcode_bottle").focus();
+  }});
+
+document.getElementById("barcode_bottle").addEventListener("keydown", (event) => {
+  let barcode_value = document.getElementById("barcode_bottle").value;
   if (event.key === "Enter") {
     const barcode_container_html = document.createElement("p");
-    barcode_container_html.textContent = `Barcode number: ${barcode_value}`;
+    barcode_container_html.textContent = `Barcode number: ${barcode_value} Gas: Sticksoff Fullmenge: 10l`;
     document.getElementById("barcode-container").appendChild(barcode_container_html);
-    document.getElementById("barcode").value = "";
+    document.getElementById("barcode_bottle").value = "";
   }
 
 

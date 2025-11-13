2fbec7c08ab802cb7a9f41de1a113c17da3f3d75
drekkg <rizzagum@gmail.com>
2025-11-13 07:07:05 +0100

    Add initial HTML structure, JavaScript functionality, and VSCode settings

---
+++ scanner_gas.js
@@ -0,0 +1,19 @@
+document.getElementById("barcode").addEventListener("keydown", (event) => {
+  let barcode_value = document.getElementById("barcode").value;
+  if (event.key === "Enter") {
+    const barcode_container_html = document.createElement("p");
+    barcode_container_html.textContent = `Barcode number: ${barcode_value}`;
+    document.getElementById("barcode-container").appendChild(barcode_container_html);
+    document.getElementById("barcode").value = "";
+  }
+
+
+
+
+
+
+  
+});
+
+// const currentDate = new Date().toISOString().slice(0, 10);
+// document.getElementById("dateCurrent").value = currentDate;

2d4bb95822a80a875a453dbd212e767dcdfdd49e
drekkg <rizzagum@gmail.com>
2025-11-14 17:07:55 +0100

    Add validation for technician and barcode input before appending to the display -stop empty elements being appended

--- scanner_gas.js
+++ scanner_gas.js
@@ -6,7 +6,7 @@ const currentDate = new Date().toISOString().slice(0, 10);
 
 document.getElementById("technician").addEventListener("keydown", (event) => {
   let technician_value = document.getElementById("technician").value;
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && technician_value) {
     const technician_html = document.createElement("h4");
     technician_html.textContent = `Techniker: ${technician_value}  Datum: ${currentDate}`;
     document.getElementById("barcode-container").appendChild(technician_html);
@@ -17,7 +17,7 @@ document.getElementById("technician").addEventListener("keydown", (event) => {
 
 document.getElementById("barcode_bottle").addEventListener("keydown", (event) => {
   let barcode_value = document.getElementById("barcode_bottle").value;
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && barcode_value) {
     const barcode_container_html = document.createElement("p");
     barcode_container_html.textContent = `Barcode number: ${barcode_value} Gas: Sticksoff Fullmenge: 10l`;
     document.getElementById("barcode-container").appendChild(barcode_container_html);
